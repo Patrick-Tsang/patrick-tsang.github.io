bb174709f418495a741688ef1f6ea12e3b20161d
Patrick Tsang <Patrick@Patricks-Mac-mini.local>
2020-05-16 12:44:30 -0400

    Vue project creation + some intro

---
+++ src/components/HomeHero.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-container fluid fill-height class="home-hero" style="max-height: 100vh;">
+        <v-layout justify-center align-center column pa-5>
+            <div class="display-3 font-weight-black white--text text-xs-center">
+                PATRICK
+            </div>
+            <div
+                class="display-3 font-weight-black white--text text-xs-center mb-3"
+            >
+                TSANG
+            </div>
+            <div class="display-1 font-weight-bold white--text text-xs-center">
+                McMaster University Software Engineering, Class of 2020
+            </div>
+            <v-btn fab class="mt-5 grey darken-4">
+                <v-icon large color="white">expand_more</v-icon>
+            </v-btn>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'HomeHero'
+};
+</script>
+
+<style scoped>
+.home-hero {
+    background: url('https://i.redd.it/q73rqtpz0vy41.jpg');
+    background-size: cover;
+    width: 100%;
+    height: 100%;
+}
+</style>
