fbcd8df25c60055b25933c725811ee3e991028a5
Patrick-Tsang <patricksmtsang@gmail.com>
2020-05-20 15:20:26 -0400

    AOS

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store';
 import vuetify from './plugins/vuetify';
 import '@babel/polyfill';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
+
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
